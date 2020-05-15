6a06214f53c1b2bf090e2dd856a0324e24d59f74
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-05-15 17:43:42 -0600

    End of context options

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends React.Component {
+  state = { language: 'english', color: 'primary' };
+
+  onLanguageChange = (language, color) => {
+    this.setState({ language, color });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i 
+            className="flag uk" 
+            onClick={() => this.onLanguageChange('english', 'primary')}
+          />
+          <i 
+            className="flag es"
+            onClick={() => this.onLanguageChange('spanish', 'red')}
+          />
+        </div>
+        <ColorContext.Provider value={this.state.color}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
